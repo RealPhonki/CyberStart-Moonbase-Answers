a29819e13037fa3b628b5ac5256aba679084edb3
Phonki <138313415+RealPhonki@users.noreply.github.com>
2023-07-18 21:44:33 -0400

    Create C4 - Passphrase Panic.py

---
+++ L05/C4 - Passphrase Panic.py
@@ -0,0 +1,18 @@
+#
+# Write a script to generate a passphrase by taking words from
+# /tmp/words.txt
+# The wordNumbers array holds three random numbers. Each number
+# corresponds to a word in words.txt. So for example
+# wordNumbers[1] is the second word in /tmp/words.txt.
+# Put a space between each word and print it out
+#
+
+
+with open("/tmp/randomwordsnumbers.txt", "r") as file:
+    data = file.read()
+
+wordNumbers = data.rstrip().split("\n")
+
+file = open("/tmp/words.txt")
+content = file.readlines()
+print(''.join([file.readlines()[int(n)].strip('\n') + ' ' for n in wordNumbers]))
